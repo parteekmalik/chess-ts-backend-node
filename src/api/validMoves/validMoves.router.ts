a5af0be6590e7f0929d107471827567ee8d7ac25
parteek malik <parteekmalik5@gmail.com>
2023-10-08 16:30:11 +0530

    LGTM

--- src/api/validMoves/validMoves.router.ts
+++ src/api/validMoves/validMoves.router.ts
@@ -1,16 +1,33 @@
-import express from 'express';
-// import findValidMoves from 'findValidMoves';
-import { findValidMoves } from 'chess-validmoves';
+import express from "express";
+import { Chess, PieceSymbol } from "chess.js";
 
-const validmoves = express.Router();
-validmoves.use(express.json());
+const live = express.Router();
+live.use(express.json());
 
-validmoves.get('/', (req, res) => {
-  res.send("only post request!!");
-});
-validmoves.post("/", (req, res) => {
+let chesMatches: { [key: string]: Chess } = { "123": new Chess() };
+
+live.get("/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("get",id);
   res.status(200);
-  res.json(findValidMoves(req.body));
+  res.json(chesMatches[id].history());
+});
+live.post("/:id", (req, res) => {
+  const { id } = req.params;
+  const { from, to } = req.body;
+  console.log(from, to);
+  let game: Chess = chesMatches[id];
+  try {
+    game.move({ from, to });
+    res.status(200).json({ error: "piece moved" });
+  } catch {
+    try {
+      game.move({ from, to, promotion: "q" as PieceSymbol });
+      res.status(200).json({ error: "piece moved" });
+    } catch {
+      res.status(200).json({ error: "Bad Request - Missing or invalid data" });
+    }
+  }
 });
 
-export default validmoves;
+export default live;
