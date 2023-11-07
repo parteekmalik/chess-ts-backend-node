f01dce2269958e9483f4fbf20cd627fe9588b0a6
parteek malik <parteekmalik5@gmail.com>
2023-11-07 15:04:44 +0530

    lgtm

--- src/api/getTime/getTime.router.ts
+++ src/api/getTime/getTime.router.ts
@@ -7,7 +7,7 @@ getTime.use(express.json());
 getTime.get("/", (req, res) => {
     console.log(req.body);
     res.status(200);
-    res.json(moment().toDate());
+    res.send(moment().toDate());
 });
 
 export default getTime;
